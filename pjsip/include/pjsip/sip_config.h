395344b3143e8cf0222f772b71216aa3ec2015a9
Michael Englehorn <michael@englehorn.com>
2021-09-25 16:27:06 -0500

    Added patches from https://github.com/pjsip/pjproject/pull/2328/files

--- pjsip/include/pjsip/sip_config.h
+++ pjsip/include/pjsip/sip_config.h
@@ -112,6 +112,22 @@ typedef struct pjsip_cfg_t
 	pj_bool_t disable_tcp_switch;
 
 	/**
+	 * Even though RFC 5922 (section 7.2) forbids the use of wildcard certificates,
+	 * some providers use them anyway and they won't or
+	 * (by the way their systems are designed) cannot change to non-wildcard certificates.
+	 *
+	 * This option will specify whether wildcard certificates are accepted.
+	 *
+	 * If this option is disabled, you can still connect to endpoints
+	 * that present wildcard certificates, provided that server verification is turned off.
+	 *
+	 * Default is 0 (no).
+	 */
+	#ifndef PJSIP_ACCEPT_TLS_WILDCARD_CERTIFICATE
+	#   define PJSIP_ACCEPT_TLS_WILDCARD_CERTIFICATE	0
+	#endif
+
+	/**
 	 * Disable automatic switching to TLS if target-URI does not use
 	 * "sips" scheme nor TLS transport, even when request-URI uses
 	 * "sips" scheme.
